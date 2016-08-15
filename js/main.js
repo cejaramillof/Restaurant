79e2b09cf615651a175640d751c4c5e3b41fffef
cejaramillof <cejaramillof@gmail.com>
2016-08-14 22:07:03 -0500

    Animar entrada del menú

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 	//Si menu esta oculto o no, sticky false = oculto
 	let sticky = false
 
+	$("#sticky-navigation").removeClass("hidden")
+	$("#sticky-navigation").slideUp(0)
+	
 	$(window).scroll(()=>{
 		const inBottom = isInBottom()
 		
@@ -19,21 +22,21 @@
 	
 	function stickNavigation(){
 		$("#description").addClass("fixed").removeClass("absolute")
-		$("#navigation").addClass("hidden")
-		$("#sticky-navigation").removeClass("hidden")
+		$("#navigation").slideUp()
+		$("#sticky-navigation").slideDown("fast")
 	}
 	
 	function unStickNavigation(){
 		$("#description").removeClass("fixed").addClass("absolute")
-		$("#navigation").removeClass("hidden")
-		$("#sticky-navigation").addClass("hidden")
+		$("#navigation").slideDown("fast")
+		$("#sticky-navigation").slideUp("fast")
 	}
 	
 	function isInBottom(){
 		const $description = $("#description")
 		const descriptionHeight = $description.height()
 	
-		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 1.5)
+		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 2)
 	}
 	
 })()
