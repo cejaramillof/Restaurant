c6374b33b36ed769e96bdb76179e7ceff7083d88
cejaramillof <cejaramillof@gmail.com>
2016-08-14 21:38:31 -0500

    Scroll al final de la pantalla

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-"use strict";!function(){$(window).scroll(function(){console.log("wiuu scroll")})}();
+"use strict";!function(){function o(){var o=$("#description"),n=o.height();return $(window).scrollTop()>$(window).height()-1.5*n}console.log($(window).height()),$(window).scroll(function(){console.log(o())})}();
--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 ;(function(){
 	
 	let sticky = false
-	
-	$(window).scroll(function(){
-		console.log("wiuu scroll")
+	console.log($(window).height())
+	$(window).scroll(()=>{
+		console.log(isInBottom())
 	})
+	
+	function isInBottom(){
+		const $description = $("#description")
+		const descriptionHeight = $description.height()
+	
+		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 1.5)
+	}
 	
 })()
