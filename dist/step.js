3f6a081939607b41082742746c8627a1ef082e2b
cejaramillof <cejaramillof@gmail.com>
2016-08-15 10:14:03 -0500

    Formulario a pasos inicio

---
+++ dist/step.js
@@ -0,0 +1,1 @@
+"use strict";!function(){function n(n){n.focus()}var t="#contact-form";$(t).find(".input").on("change",function(t){var o=$(t.target),c=o.next();n(c)})}();
---
+++ js/steps.js
@@ -0,0 +1,25 @@
+;(function(){
+	const selector = "#contact-form"
+	
+	$(selector).find(".input").on("change",(ev)=>{
+		let $input = $(ev.target)
+		
+		let $next_input = $input.next()
+		
+		enfocar_siguiente_input($next_input)
+		
+	})
+	// Helpers
+	function validar_formulario(){
+		
+	}
+	
+	function es_valido_formulario(){
+		
+	}
+	
+	function enfocar_siguiente_input($next_input){
+		$next_input.focus()
+	}
+	
+})()
