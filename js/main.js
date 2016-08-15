f1646ec9e2b5464a929f87f38a7666a26935e6bb
cejaramillof <cejaramillof@gmail.com>
2016-08-15 00:21:00 -0500

    Mover la galería con javaScript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 ;(function(){
 	//Si menu esta oculto o no, sticky false = oculto
 	let sticky = false
-
+	let currentPosition = 0
+	
 	$("#sticky-navigation").removeClass("hidden")
 	$("#sticky-navigation").slideUp(0)
+	
+	setInterval(()=>{
+		currentPosition++
+		$("#gallery .inner").css({
+			left: "-"+currentPosition*100+"%"
+		})
+	},4000)
 	
 	$(window).scroll(()=>{
 		const inBottom = isInBottom()
