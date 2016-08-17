e0a6efb6f22c67c843b8dd9ca16ee9d667700949
cejaramillof <cejaramillof@gmail.com>
2016-08-17 15:33:57 -0500

    Obtener localización del usuario

--- js/maps.js
+++ js/maps.js
@@ -1,6 +1,26 @@
 ;(function(){
+	
+	class UserLocation{
+		static get(callback){
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition((location)=>{
+					callback({
+						lat: location.coords.latitude,
+						lng: location.coords.longitude
+					})
+				})
+			}else{
+				alert("No pudimos detectar el lugar en el que te encuentras.")
+			}
+		}		
+	}
+	
 	google.maps.event.addDomListener(window,"load",()=>{
 		const map = new google.maps.Map(
-			document.getElementById('map'), {center: {lat: 6.234076, lng: -75.586069}, zoom: 16})
+			document.getElementById('map'), {center: {lat: 6.234076, lng: -75.586069}, zoom: 15})
+		
+		UserLocation.get((coords)=>{
+			console.log(coords)
+		})
 	})
 })()
