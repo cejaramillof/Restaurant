2cb36cac9424f0211832732a1d69586392b85159
cejaramillof <cejaramillof@gmail.com>
2016-08-18 15:38:32 -0500

    Guardar en cache

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
+const CACHE_NAME = "restaurantefacilito"
+const cache_urls = ["/offline/view.html","/offline/style.css","/offline/map.png"]
 self.addEventListener("install",function(ev){
-	console.log("Service Worker instalada.")
+	caches.open(CACHE_NAME)
+				.then(function(cache){
+					console.log("Cache opened")
+					return cache.addAll(cache_urls)
+				})
 })
