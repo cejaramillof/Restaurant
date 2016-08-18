aae881c1a27e94557b4a8c7888c41887d951eb93
cejaramillof <cejaramillof@gmail.com>
2016-08-18 16:39:31 -0500

    Evitar cache del navegador y autolimpiar cache

--- sw.js
+++ sw.js
@@ -1,11 +1,26 @@
-const CACHE_NAME = "restaurantefacilito"
+const CACHE_NAME = "restaurantefacilito_v1"
 const cache_urls = ["/offline/view.html","/offline/style.css","/offline/map.png"]
+
 self.addEventListener("install",function(ev){
 	caches.open(CACHE_NAME)
 				.then(function(cache){
 					console.log("Cache opened")
 					return cache.addAll(cache_urls)
 				})
+})
+
+self.addEventListener("activate",function(ev){
+	ev.waitUntil(
+		caches.keys().then(function(cache_names){
+			return Promise.all(
+				cache_names.map(function(cache_name){
+					if(CACHE_NAME !== cache_name){
+						return caches.delete(cache_name)
+					}
+				})
+			)
+		})
+	)
 })
 
 self.addEventListener("fetch",function(ev){
@@ -16,6 +31,10 @@ self.addEventListener("fetch",function(ev){
 							return response
 						}
 						return fetch(ev.request)
+					}).catch(function(err){
+						if(ev.request.mode == "navigate"){
+							return caches.match("/offline/view.html")
+						}
 					})
 	)
 })
