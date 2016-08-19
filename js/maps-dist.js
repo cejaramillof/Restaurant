98c0937d00798f6a41f391e3b825d54b0eac6fbe
cejaramillof <cejaramillof@gmail.com>
2016-08-18 23:23:21 -0500

    Concatenar archivos

--- js/maps-dist.js
+++
@@ -1,1 +0,0 @@
-"use strict";!function(){google.maps.event.addDomListener(window,"load",function(){new google.maps.Map(document.getElementById("map"),{center:{lat:6.234076,lng:-75.586069},zoom:15})})}();
